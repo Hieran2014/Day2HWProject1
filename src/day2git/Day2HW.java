1edc2c09b79cf2a049e02b7def9300fe0a570a33
GYTumlisan <gymoa2011@gmail.com>
2023-04-08 09:59:06 -0700

    More Info added (add() and divid())

--- src/day2git/Day2HW.java
+++ src/day2git/Day2HW.java
@@ -8,5 +8,17 @@ public class Day2HW {
 		
 		System.out.println("Remote repository");
 	}
+	
+	// Stash
+	public void add(int a, int b) {
+		
+		System.out.println(a + b);
+	}
+	
+	//Push
+	public void divid(int x, int y) {
+		
+		System.out.println(x/y);
+	}
 
 }
